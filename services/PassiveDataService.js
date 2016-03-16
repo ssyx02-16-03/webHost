f253249e4c477856c5d237b5797103c365cc8256
Oskar Henriksson <Oskar Henriksson>
2016-03-16 19:18:55 +0100

    freshest values of each webserver_package type is always saved, ready to deliver when client asks

--- services/PassiveDataService.js
+++ services/PassiveDataService.js
@@ -1,6 +1,10 @@
 define([], function() {
   var topic = '/topic/elvisSnapShot'; //IRL should be '/topic/webserver_package'
 
+//implement a hashSet for each webserver_package
+var latestData = {};
+latestData['webserver_test'] = 'testData';
+
 	function start(angelClient, socketIOServer) {
 		angelClient.subscribe(topic, function(body, headers) {
       console.log('passiveDataService: Data recieved');
@@ -16,10 +20,14 @@ define([], function() {
       //throw "package_type or package_data was NULL"; //commented out for testing purposes
     }
     var eventName = 'webserver_' +type;
+    latestData[eventName] = data; //save latest data if user request it
     console.log('passiveDataService: type:', eventName, '\tdata:', data);
-    socketIO.emit(eventName, data);
-	}
+    socketIO.on(eventName, function(callbackFunc){ //subscribe to everything we got data on.
+      callbackFunc(latestData[eventName]); //give the user the latest and greatest of our data
+    });
 
+    console.log(latestData);
+	}
   return {
     start:start
   }
