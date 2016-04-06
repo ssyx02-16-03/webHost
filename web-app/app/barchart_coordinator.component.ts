12a5815b2aae6e3f656a79c264f83bd9c8792e67
sderlund <asasoderlund@dhcp-181223.eduroam.chalmers.se>
2016-04-06 10:40:21 +0200

    New bar chart for coordinator view

---
+++ web-app/app/barchart_coordinator.component.ts
@@ -0,0 +1,24 @@
+/**
+ * Created by edvard on 2016-03-01.
+ */
+
+import {Component, OnInit} from 'angular2/core';
+
+//import * as d3 from 'd3';
+
+import {barchart_coordinator} from './barchart_coordinator.ts'
+
+@Component({
+    template: `
+		<h2>Stapeldiagram</h2>
+		<d3></d3>
+		`,
+    directives: [barchart_coordinator]
+})
+
+export class barchart_coordinatorComponent implements OnInit {
+
+    ngOnInit() {
+        barchart_coordinator.draw();
+    }
+}
