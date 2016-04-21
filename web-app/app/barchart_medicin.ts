c09c5defa5823872b9f54b6dcba86eab6e9b1a2f
Oskar Henriksson <o.henriksson@gmail.com>
2016-04-21 21:02:02 +0200

    Barchart fix

--- web-app/app/barchart_medicin.ts
+++ web-app/app/barchart_medicin.ts
@@ -9,21 +9,21 @@ import * as d3 from 'd3';
 @Component({
     selector: '.medbarchart',
     template: `
-        <p class='barchart_p' style="font-size: 200%; margin: 0 auto; height:10%;">Patientantal: {{nPatients}}</p>
+        <h3 id="barchart_totNumber" style="margin: 0 auto; height:10%;">Patientantal: </h3>
+
 		<svg class='barchart_medicine' style="display:block; margin:0 auto;"></svg>
 		`
 })
 
 
 export class barchart_medicin {
+
     private static scaleSVG(svg,width,height,endpoints){
         svg.attr("viewBox", endpoints[0] +" " +endpoints[1] +" " +endpoints[2] +" " +endpoints[3]);
         svg.attr("preserveAspectRatio","xMaxYMax");
         svg.attr("height",height +"%");
         svg.attr("width",width +"%");
     }
-
-
 
     public static draw(rawData){
         var rawData= rawData.bars;
@@ -43,6 +43,9 @@ export class barchart_medicin {
 
         var nPatients = jsonData.total_patients;
 
+        h3_number = document.getElementById("barchart_totNumber");
+        h3_number.textContent = "Patientantal: "+nPatients;
+
         var max = nPatients;
         var width = 500,
             chartWidth = 200,
@@ -53,8 +56,6 @@ export class barchart_medicin {
             fontSize = 11,
             legendSpace = height / 20,
             legendSize = legendSpace / 2;
-
-        //document.write(Object.keys(jsonData).length);
 
         var color = ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]); //finns enbart med för att loopen i legend ska bli rätt..
 
@@ -78,7 +79,7 @@ export class barchart_medicin {
             .scale(y)
             .orient("left")
             .ticks(10)
-            .outerTickSize(2)
+            .outerTickSize(1)
             .tickFormat(function(d) { //eliminate decimal numbers
                 if(d%1 == 0){
                     return d;
@@ -89,14 +90,14 @@ export class barchart_medicin {
             .append("g")
             .attr("id","data")
             .attr("id","data")
-            .attr("transform", "translate(" + 50 + "," + 50 + ")");
+            .attr("transform", "translate(50,50)"); //rotate
 
         bar.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + chartHeight + ")")
             .call(xAxis)
             .selectAll("text")
-            .attr("font-size", fontSize * 1.2);
+            .attr("font-size", fontSize * 1.0);
 
         bar.append("g")
             .attr("class", "y axis")
@@ -121,11 +122,8 @@ export class barchart_medicin {
                     .attr("y", i * legendSpace + (height - chartHeight) / 2)
                     .attr("width", legendSize)
                     .attr("height", legendSize);
-                if ( i == 0) {
-                    rect.style("fill", "none")
-                        .style("stroke", color_hash[0][1])
-                        .style("stroke-dasharray", ("3, 3"))
-                        .style("stroke-width", "1.75px");
+                if(i == 0) {
+                      strokeEffect(rect);
                 }
                 else {
                     rect.style("fill", color_hash[i][1]);
@@ -137,55 +135,36 @@ export class barchart_medicin {
                     .text(color_hash[i][0]);
             });
 
-
           var barBox = bar.append("g")
               .attr("id","chartArea");
 
+        // -----------TRIAGE STATUS ----------------------
+        var triage = [];
+        triage[0] = jsonData.blue;
+        triage[1] = jsonData.green;
+        triage[2] = jsonData.yellow;
+        triage[3]  = jsonData.orange;
+        triage[4]  = jsonData.red;
 
-          // -----------TRIAGE STATUS ----------------------
-          var triage = [];
-          triage[0] = jsonData.blue;
-          triage[1] = jsonData.green;
-          triage[2] = jsonData.yellow;
-          triage[3]  = jsonData.orange;
-          triage[4]  = jsonData.red;
+        var xCoord = 1 * barSpace - barWidth;
+        var lastBox = drawPile(triage,barBox,y, chartHeight,barWidth, xCoord, 4);
 
-          var xCoord = 1 * barSpace - barWidth;
-          drawPile(triage,barBox,y, chartHeight,barWidth, xCoord, 4);
+        //---------- INKOMMANDE
+        var incoming:number = jsonData.incoming;
+        var incWidth = barWidth+2*barSpace;
+        var incHeight = chartHeight -y(incoming);
+        var incColor = "";
+        var block = new Block(barBox, lastBox.x, lastBox.y-incHeight, incWidth, incHeight, incColor, incoming);
+        strokeEffect(block.svgBlock);
 
         //----------------------PATIENTSTATUS--------------------------
-        /*//Total siffra
-        bar.append("text")
-            .attr("x", 2 * barSpace - barWidth / 2 - fontSize )
-            .attr("y", y(jsonData[0].totalt) - 5 )
-            .text( jsonData[0].totalt)
-            .attr("font-size", fontSize * 1.5)
-            .attr("font-weight", "bold");*/
-
         var patientStatus = [];
-        patientStatus[0] = jsonData.incoming;
-        patientStatus[2] = jsonData.has_doctor;
-        patientStatus[1] = jsonData.no_doctor;
-        patientStatus[3] = jsonData.klar;
-
+        patientStatus[0] = jsonData.no_doctor;
+        patientStatus[1] = jsonData.has_doctor;
+        patientStatus[2] = jsonData.klar;
 
         var xCoord = 2 * barSpace - barWidth;
-        drawPile(patientStatus, barBox, y,chartHeight,barWidth ,xCoord,0);
-
-        /* incomming style
-        barBox.data(staplar)
-            .each(function(d, i) {
-                barBox.append("rect")
-                    .attr("x", (1 + i) * barSpace - barWidth)
-                    .attr("y", y(incoming) - chartHeight + y(has_doctor) - chartHeight + y(no_doctor) - chartHeight + y(done_patients))
-                    .attr("height", chartHeight - y(incoming))
-                    .attr("width", 2 * barWidth / 2)
-                    .style("stroke", color_hash[0][1])
-                    .style("fill", "none")
-                    .style("stroke-dasharray", ("8, 8"), "stroke-width", "3px" )
-                    .style();
-            });
-            */
+        drawPile(patientStatus, barBox, y,chartHeight,barWidth ,xCoord,1);
 
         // -----------RUMSFÖRDELNING ----------------------
         var roomArray = [];
@@ -197,8 +176,35 @@ export class barchart_medicin {
         var xCoord = 3 * barSpace - barWidth;
         drawPile(roomArray, barBox, y, chartHeight, barWidth ,xCoord,9);
     }//draw()
-
 }
+
+function strokeEffect(svg){
+  svg.style("fill", "none")
+      .style("stroke", color_hash[0][1])
+      .style("stroke-dasharray", ("2, 2"))
+      .style("stroke-width", "1.75px");
+}
+
+function paintBlock(parent,x:number,y:number,width:number,height:number,color:string){
+  var block = parent.append("rect")
+      .attr("x", x)
+      .attr("y", y)
+      .attr("width", width)
+      .attr("height", height)
+      .attr("fill", color);
+  return block;
+}
+
+function paintText(parent,x,y,fontColor,fontSize,text){
+  var textField = parent.append("text")
+      .attr("x", x)
+      .attr("y", y + fontSize)
+      .attr("font-size", fontSize)
+      .attr("fill", fontColor)
+      .text(text);
+  return textField;
+}
+
 
 function drawPile(dataArray, parent, yAxis, chartHeight:number, barWidth:number, xCoord:number, colorOffset:number){
     Block lastBox = null;
@@ -212,10 +218,11 @@ function drawPile(dataArray, parent, yAxis, chartHeight:number, barWidth:number,
           xCoord, yCoord,
           barWidth, boxHeight, color_hash[i+colorOffset][1], dataArray[i]);
     }
+    return lastBox;
 }
 
 var color_hash = {
-    0 : ["inkommande", "white"],
+    0 : ["inkommande", "black"],
     1 : ["opåtittade", "lightgrey"],
     2 : ["påtittade", "grey"]
     3 : ["klara", "black"],
@@ -232,8 +239,6 @@ var color_hash = {
 
 var staplar = (["Priofärg", "Läkarstatus", "Plats"]);
 
-
-
 class Block{
     parent; //parent Div
     x:number
@@ -245,7 +250,7 @@ class Block{
     nOfPatients:number;
 
     fontColor:string;
-    svgBlock;
+    svgBlock; //blockDiv
 
     constructor(parent,x,y, width:number,height:number,color:string, nOfPatients:number){
         this.x = x;
@@ -258,32 +263,14 @@ class Block{
         this.nOfPatients = nOfPatients;
         this.fontColor = "black";
 
-        console.log("y",y);
+        if(this.color == this.fontColor){
+          this.fontColor = "white";
+        }
 
         if(nOfPatients > 0){
-          this.svgBlock = this.paintBlock();
-          this.paintText();
+          this.svgBlock = paintBlock(parent,x,y,width,height,color);
+          paintText(parent,x,y,this.fontColor,this.fontSize,nOfPatients);
         }
         return this;
     }
-
-    paintBlock(){
-      var block = this.parent.append("rect")
-          .attr("x", this.x)
-          .attr("y", this.y)
-          .attr("width", this.width)
-          .attr("height", this.height)
-          .attr("fill", this.color);
-      return block;
-    }
-
-    paintText(){
-      var textField = this.parent.append("text")
-          .attr("x", this.x)
-          .attr("y", this.y + this.fontSize)
-          .attr("font-size", this.fontSize)
-          .attr("fill", this.fontColor)
-          .text(this.nOfPatients);
-    }
-
 }
