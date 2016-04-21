b53ce650690d48fed9210a01b8adaa6c63ea14b2
Oskar Henriksson <o.henriksson@gmail.com>
2016-04-21 13:04:30 +0200

    Barcharts improved, dashed top still need attention

--- web-app/app/barchart_medicin.ts
+++ web-app/app/barchart_medicin.ts
@@ -24,7 +24,6 @@ export class barchart_medicin {
     }
 
 
-    private static nPatients;
 
     public static draw(rawData){
         var rawData= rawData.bars;
@@ -42,9 +41,9 @@ export class barchart_medicin {
         this.scaleSVG(svg,90,90,[0,30,400,300]);
         d3.select(".barchart_p").style("width","80%");
 
-        this.nPatients = jsonData.total_patients;
+        var nPatients = jsonData.total_patients;
 
-        var max = this.nPatients;
+        var max = nPatients;
         var width = 500,
             chartWidth = 200,
             height = 300,
@@ -59,23 +58,8 @@ export class barchart_medicin {
 
         var color = ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]); //finns enbart med för att loopen i legend ska bli rätt..
 
-        var color_hash = {
-            0 : ["inkommande", "lightgrey"],
-            1 : ["påtittade", "grey"],
-            2 : ["opåtittade", "lightgrey"],
-            3 : ["klara", "black"],
-            4 : ["blå", "blue"],
-            5 : ["grön", "green"],
-            6 : ["gul", "yellow"],
-            7 : ["orange", "orange"],
-            8 : ["röd", "red"],
-            9 : ["rum", "#236B8E"],
-            10 : ["inre väntrum", "#5D92B1"],
-            11 : ["undersökning", "#A4D3EE"],
-            12 : ["annan plats", "#B0E2FF"]
-        };
-
-        var staplar = (["Priofärg", "Läkarstatus", "Plats"]);
+        var chart = d3.select(".barchart_medicine");
+        chart.selectAll("*").remove(); //delete garbage
 
         var x = d3.scale.ordinal()
             .domain(staplar)
@@ -101,13 +85,6 @@ export class barchart_medicin {
                 }
             });
 
-        //Chart
-        var chart = d3.select(".barchart_medicine")
-            //.attr("width", width)
-            //.attr("height", height);
-        chart.selectAll("*").remove(); //delete garbage
-
-
         var bar = chart
             .append("g")
             .attr("id","data")
@@ -129,7 +106,7 @@ export class barchart_medicin {
         //Legend
         var legend = chart.append("g")
             .attr("class", "legend")
-            .attr("x", chartWidth+barWidth*3)
+            .attr("x", chartWidth+barWidth*4)
             .attr("y", 25)
             .attr("height", 100)
             .attr("width", 100);
@@ -161,6 +138,21 @@ export class barchart_medicin {
             });
 
 
+          var barBox = bar.append("g")
+              .attr("id","chartArea");
+
+
+          // -----------TRIAGE STATUS ----------------------
+          var triage = [];
+          triage[0] = jsonData.blue;
+          triage[1] = jsonData.green;
+          triage[2] = jsonData.yellow;
+          triage[3]  = jsonData.orange;
+          triage[4]  = jsonData.red;
+
+          var xCoord = 1 * barSpace - barWidth;
+          drawPile(triage,barBox,y, chartHeight,barWidth, xCoord, 4);
+
         //----------------------PATIENTSTATUS--------------------------
         /*//Total siffra
         bar.append("text")
@@ -170,15 +162,17 @@ export class barchart_medicin {
             .attr("font-size", fontSize * 1.5)
             .attr("font-weight", "bold");*/
 
-        var incoming = jsonData.incoming;
-        var has_doctor = jsonData.has_doctor;
-        var no_doctor = jsonData.no_doctor;
-        var done_patients = jsonData.klar;
-        
-        //Inkommande
-        var barBox = bar.append("g")
-            .attr("id","chartArea");
+        var patientStatus = [];
+        patientStatus[0] = jsonData.incoming;
+        patientStatus[2] = jsonData.has_doctor;
+        patientStatus[1] = jsonData.no_doctor;
+        patientStatus[3] = jsonData.klar;
 
+
+        var xCoord = 2 * barSpace - barWidth;
+        drawPile(patientStatus, barBox, y,chartHeight,barWidth ,xCoord,0);
+
+        /* incomming style
         barBox.data(staplar)
             .each(function(d, i) {
                 barBox.append("rect")
@@ -186,240 +180,110 @@ export class barchart_medicin {
                     .attr("y", y(incoming) - chartHeight + y(has_doctor) - chartHeight + y(no_doctor) - chartHeight + y(done_patients))
                     .attr("height", chartHeight - y(incoming))
                     .attr("width", 2 * barWidth / 2)
-                    .style("stroke-dasharray", ("8, 8"))
                     .style("stroke", color_hash[0][1])
                     .style("fill", "none")
-                    .style("stroke-width", "3px");
+                    .style("stroke-dasharray", ("8, 8"), "stroke-width", "3px" )
+                    .style();
             });
-
-        barBox.selectAll("g").data(staplar)
-            .each(function(d, i) {
-                var inktext = barBox.append("text")
-                    .attr("x", (1 + i) * barSpace - barWidth)
-                    .attr("y", y(incoming) - chartHeight + y(has_doctor) - chartHeight + y(no_doctor) - chartHeight + y(done_patients) + fontSize)
-                    .attr("font-size", fontSize);
-                if (incoming != 0) {
-                    inktext.text(incoming)
-                }
-            });
-
-        //Opåtittade
-        barBox.append("rect")
-            .attr("x", barSpace - barWidth)
-            .attr("y", y(no_doctor))
-            .attr("height", chartHeight - y(no_doctor))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[2][1]);
-
-        var otitttext = barBox.append("text")
-            .attr("x", barSpace - barWidth)
-            .attr("y", y(no_doctor) + fontSize)
-            .attr("font-size", fontSize);
-        if (no_doctor != 0) {
-            otitttext.text(no_doctor) }
-
-        //Påtittade
-        barBox.append("rect")
-            .attr("x", barSpace - barWidth)
-            .attr("y", y(has_doctor) - chartHeight + y(no_doctor))
-            .attr("height", chartHeight - y(has_doctor))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[1][1]);
-
-        var titttext = barBox.append("text")
-            .attr("x", barSpace - barWidth)
-            .attr("y", y(has_doctor) - chartHeight + y(no_doctor) + fontSize )
-            .attr("font-size", fontSize);
-        if (has_doctor != 0) {
-            titttext.text(has_doctor) }
-
-        //Klara
-        barBox.append("rect")
-            .attr("x", barSpace - barWidth)
-            .attr("y", y(done_patients) - chartHeight + y(has_doctor) - chartHeight + y(no_doctor))
-            .attr("height", chartHeight - y(done_patients) )
-            .attr("width", barWidth)
-            .attr("fill", color_hash[String(3)][1]);
-
-        var klartext = barBox.append("text")
-            .attr("x", barSpace - barWidth)
-            .attr("y", y(done_patients) - chartHeight + y(has_doctor) - chartHeight + y(no_doctor) + fontSize)
-            .attr("font-size", fontSize)
-            .attr("fill", "red");
-        if (done_patients != 0) {
-            klartext.text(done_patients)}
-
-
-        // -----------TRIAGE STATUS ----------------------
-        var triage_blue = jsonData.blue;
-        var triage_green  = jsonData.green;
-        var triage_yellow  = jsonData.yellow;
-        var triage_orange  = jsonData.orange;
-        var triage_red  = jsonData.red;
-        
-        //Blåa
-        barBox.append("rect")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_blue))
-            .attr("height", chartHeight - y(triage_blue))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[String(4)][1]);
-
-
-        var bluetext = barBox.append("text")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_blue) + fontSize)
-            .attr("font-size", fontSize)
-            .attr("fill", "black");
-        if (triage_blue != 0 ) {
-            bluetext.text(triage_blue) }
-
-        //Gröna
-        barBox.append("rect")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_green) - chartHeight + y(triage_blue))
-            .attr("height", chartHeight - y(triage_green))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[String(5)][1]);
-
-        var greentext = barBox.append("text")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_green) - chartHeight + y(triage_blue) + fontSize)
-            .attr("font-size", fontSize)
-            .attr("fill", "black");
-        if (triage_green != 0) {
-            greentext.text(triage_green) }
-
-        //Gula
-        barBox.append("rect")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_yellow) - chartHeight + y(triage_blue) - chartHeight + y(triage_green))
-            .attr("height", chartHeight - y(triage_yellow))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[6][1]);
-
-        var yelltext = barBox.append("text")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_yellow) - chartHeight + y(triage_blue) - chartHeight + y(triage_green) + fontSize)
-            .attr("font-size", fontSize)
-            .attr("fill", "black");
-        if (triage_yellow != 0) {
-            yelltext.text(triage_yellow) }
-
-        //Orangea
-        barBox.append("rect")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_orange) - chartHeight + y(triage_blue) - chartHeight + y(triage_green) - chartHeight + y(triage_yellow))
-            .attr("height", chartHeight - y(triage_orange))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[7][1]);
-
-        var orantext = barBox.append("text")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_orange) - chartHeight + y(triage_blue) - chartHeight + y(triage_green) - chartHeight + y(triage_yellow) + fontSize)
-            .attr("font-size", fontSize)
-            .attr("fill", "black");
-        if (triage_orange != 0) {
-                orantext.text(triage_orange) }
-
-        //Röda
-        barBox.append("rect")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_red) - chartHeight + y(triage_blue) - chartHeight + y(triage_green) - chartHeight + y(triage_yellow) - chartHeight + y(triage_orange))
-            .attr("height", chartHeight - y(triage_red))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[8][1]);
-
-        var redtext = barBox.append("text")
-            .attr("x", 2 * barSpace - barWidth)
-            .attr("y", y(triage_red) - chartHeight + y(triage_blue) - chartHeight + y(triage_green) - chartHeight + y(triage_yellow) - chartHeight + y(triage_orange) + fontSize)
-            .attr("font-size", fontSize)
-            .attr("fill", "black");
-        if (triage_red != 0) {
-            redtext.text(triage_red); }
-
-
+            */
 
         // -----------RUMSFÖRDELNING ----------------------
-        var rooms_here = jsonData.rooms_here;
-        var inner_waiting = jsonData.inner_waiting_room;
-        var at_examination = jsonData.at_examination;
-        var rooms_elsewhere = jsonData.rooms_elsewhere;
-        if(rooms_here == undefined){
-            rooms_here = 0;
-        }
-        if(inner_waiting == undefined){
-            inner_waiting = 0;
-        }
-        if(at_examination == undefined){
-            at_examination = 0;
-        }
-        if(rooms_elsewhere == undefined){
-            rooms_elsewhere = 0;
-        }
-        
-        //Rum
-        barBox.append("rect")
-            .attr("x", 3 * barSpace - barWidth)
-            .attr("y", y(rooms_here))
-            .attr("height", chartHeight - y(rooms_here))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[String(9)][1]);
+        var roomArray = [];
+        roomArray[0] = jsonData.rooms_here;
+        roomArray[1] = jsonData.inner_waiting_room;
+        roomArray[2] = jsonData.at_examination;
+        roomArray[3] = jsonData.rooms_elsewhere;
 
-        var roomtext = barBox.append("text")
-            .attr("x", 3 * barSpace - barWidth)
-            .attr("y", y(rooms_here) + fontSize)
-            .attr("font-size", fontSize);
-        if (rooms_here != 0) {
-            roomtext.text(rooms_here) }
-
-        //Väntrum
-        barBox.append("rect")
-            .attr("x", 3 * barSpace - barWidth)
-            .attr("y", y(inner_waiting) - chartHeight + y(rooms_here))
-            .attr("height", chartHeight - y(inner_waiting))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[String(10)][1]);
-
-        var waittext = barBox.append("text")
-            .attr("x", 3 * barSpace - barWidth)
-            .attr("y", y(inner_waiting) - chartHeight + y(rooms_here) + fontSize)
-            .attr("font-size", fontSize);
-        if (inner_waiting != 0) {
-            waittext.text(inner_waiting) }
-
-
-        //Undersökning
-        barBox.append("rect")
-            .attr("x", 3 * barSpace - barWidth)
-            .attr("y", y(at_examination) - chartHeight + y(inner_waiting) - chartHeight + y(rooms_here))
-            .attr("height", chartHeight - y(at_examination))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[String(11)][1]);
-
-        var extext = barBox.append("text")
-            .attr("x", 3 * barSpace - barWidth)
-            .attr("y", y(at_examination) - chartHeight + y(inner_waiting) - chartHeight + y(rooms_here) + fontSize)
-            .attr("font-size", fontSize);
-        if (at_examination != 0) {
-            extext.text(at_examination) }
-
-
-        //Annat
-        barBox.append("rect")
-            .attr("x", 3 * barSpace - barWidth)
-            .attr("y", y(rooms_elsewhere) - chartHeight + y(rooms_here) - chartHeight + y(inner_waiting) - chartHeight + y(at_examination))
-            .attr("height", chartHeight - y(rooms_elsewhere))
-            .attr("width", barWidth)
-            .attr("fill", color_hash[String(12)][1]);
-
-        var elsetext = barBox.append("text")
-            .attr("x", 3 * barSpace - barWidth)
-            .attr("y", y(rooms_elsewhere) - chartHeight + y(rooms_here) - chartHeight + y(inner_waiting) - chartHeight + y(at_examination) + fontSize)
-            .attr("font-size", fontSize);
-        if (rooms_elsewhere != 0) {
-            elsetext.text(rooms_elsewhere) }
-
-
+        var xCoord = 3 * barSpace - barWidth;
+        drawPile(roomArray, barBox, y, chartHeight, barWidth ,xCoord,9);
     }//draw()
+
+}
+
+function drawPile(dataArray, parent, yAxis, chartHeight:number, barWidth:number, xCoord:number, colorOffset:number){
+    Block lastBox = null;
+    var yCoord = yAxis(dataArray[dataArray.length-1]);
+    for(var i=dataArray.length-1; i >= 0; i--){
+        var boxHeight = chartHeight-yAxis(dataArray[i]);
+        if(lastBox != null){
+            yCoord = lastBox.y-boxHeight);
+        }
+        lastBox = new Block(parent,
+          xCoord, yCoord,
+          barWidth, boxHeight, color_hash[i+colorOffset][1], dataArray[i]);
+    }
+}
+
+var color_hash = {
+    0 : ["inkommande", "white"],
+    1 : ["opåtittade", "lightgrey"],
+    2 : ["påtittade", "grey"]
+    3 : ["klara", "black"],
+    4 : ["röd", "red"],
+    5:  ["orange", "orange"],
+    6:  ["gul", "yellow"],
+    7:  ["grön", "green"],
+    8:  ["blå", "blue"],
+    9:  ["annan plats", "#B0E2FF"],
+    10: ["undersökning", "#A4D3EE"],
+    11: ["inre väntrum", "#5D92B1"],
+    12: ["rum", "#236B8E"]
+};
+
+var staplar = (["Priofärg", "Läkarstatus", "Plats"]);
+
+
+
+class Block{
+    parent; //parent Div
+    x:number
+    y:number
+    width:number;
+    height:number;
+    fontSize:number;
+    color:string;
+    nOfPatients:number;
+
+    fontColor:string;
+    svgBlock;
+
+    constructor(parent,x,y, width:number,height:number,color:string, nOfPatients:number){
+        this.x = x;
+        this.y = y;
+        this.parent = parent;
+        this.width = width;
+        this.height = height;
+        this.fontSize = 11;
+        this.color = color;
+        this.nOfPatients = nOfPatients;
+        this.fontColor = "black";
+
+        console.log("y",y);
+
+        if(nOfPatients > 0){
+          this.svgBlock = this.paintBlock();
+          this.paintText();
+        }
+        return this;
+    }
+
+    paintBlock(){
+      var block = this.parent.append("rect")
+          .attr("x", this.x)
+          .attr("y", this.y)
+          .attr("width", this.width)
+          .attr("height", this.height)
+          .attr("fill", this.color);
+      return block;
+    }
+
+    paintText(){
+      var textField = this.parent.append("text")
+          .attr("x", this.x)
+          .attr("y", this.y + this.fontSize)
+          .attr("font-size", this.fontSize)
+          .attr("fill", this.fontColor)
+          .text(this.nOfPatients);
+    }
+
 }
